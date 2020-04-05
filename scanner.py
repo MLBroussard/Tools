f68b475dc2b4a4d8f999dce7cd499364b0761028
MLBroussard <michellelbroussard@gmail.com>
2020-04-05 12:43:49 -0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -18,7 +18,7 @@ print("Time started: "+str(datetime.now()))
 print("-" * 50)
 
 try:
-	for port in range(50,85):
+	for port in range(50,85): #Edit this later...
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
 		result = s.connect_ex((target,port)) #returns an error indicator
